c38285cb15c6940a7cbfe86f6f3f07be8d1b9481
Lorianns <lorianns615@gmail.com>
2016-11-29 22:02:54 -0400

    - created a StockHistoryTask class

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/StockHistoryTask.java
@@ -0,0 +1,10 @@
+package com.sam_chordas.android.stockhawk;
+
+/**
+ * Created by lorianns on 11/6/16.
+ */
+
+public class StockHistoryTask {
+    //startDate, which is not sent in YYYY-MM-DD format
+    //https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22GOOG%22%20and%20startDate%20%3D%20%222016-03-03%22%20and%20endDate%20%3D%20%222016-6-222010-03-10%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
+}
